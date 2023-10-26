0637d827c644a5ac304be66187f34f23865fbdc4
Tomás <troldanborjas@mail.austral.edu.ar>
2023-10-26 18:19:48 -0300

    Implemented almost all methods

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -6,16 +6,49 @@ public interface Graph<T> {
      * @param x : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL
      */
     void addVertex(T x);
+    /**
+     *removeVertex: this method should delete a vertex that should already exist in the structure. If not, this method should throw an exception
+     * @param x : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL.
+     */
     void removeVertex(T x);
+    /**
+     *hasVertex: this method should return whether a vertex is in the structure.
+     * @param v : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL.
+     */
     boolean hasVertex(T v);
+    /**
+     *getVertexes: this method should return a list containing all vertices
+     */
     List<T> getVertexes();
-
-    void addEdge(T v, T w);
+    /**
+     *addEdge: this method should connect two vertices that should already exist in the structure. If not, this method should do nothing
+     * @param v : index that should point to a vertex from the graph. SHOULD NOT BE NULL
+     * @param w : index that should point to a vertex from the graph. SHOULD NOT BE NULL
+     */
+    void addEdge(T v, T w, int weight);
+    /**
+     *removeEdge: this method should remove the edge between two vertices that should already exist in the structure. If not, this method should do nothing
+     * @param v : position that should point to a vertex from the graph. SHOULD NOT BE NULL
+     * @param w : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL
+     */
     void removeEdge(T v, T w);
+    /**
+     *hasEdge: this method should return true if there exists an edge between two vertices that should already exist in the structure. If not, returns false
+     * @param v : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL
+     * @param w : T type object that should represent a vertex from the graph. SHOULD NOT BE NULL
+     */
     boolean hasEdge(T v, T w);
-
+    /**
+     *order: this method should return the graph's order
+     */
     int order();
+    /**
+     *alpha: this method should return the number of edges in the graph
+     */
     int alpha();
-
+    /**
+     *getAdjacencyList: this method should return the adjacency list of the vertex "v".
+     * @param v : vertex from which the adjacency list will be gotten, that should be in the graph . SHOULD NOT BE NULL
+     */
     List<T> getAdjacencyList(T v);
 }
--- src/main/java/PrimGraph.java
+++ src/main/java/PrimGraph.java
@@ -1,15 +1,14 @@
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 // Realizar la implementación de la interfaz Graph, para grafos no dirigidos y ponderados
 // con una Matriz de costos. Calcular el orden de cada operación.
 // Implementar el algoritmo de Prim.
 public class PrimGraph<T> implements Graph<T>{
-    private final int size;
-    private final Comparator<T> comparator;
-    private int[][] costMatrix;
+    private final int size; //Matrix row and column size
+    private final Comparator<T> comparator; //Comparator we will be using with Prim algorithm
+    private int[][] costMatrix; //Cost Matrix
 
     public PrimGraph(@NotNull Comparator<T> comparator){
         this.comparator = comparator;
@@ -21,54 +20,116 @@ public class PrimGraph<T> implements Graph<T>{
         this.size = size;
         this.costMatrix = new int[size][size];
     }
-
+    //Edges should have values greater than or equal to 0, so we use -1 to represent that there is no edge between v and w
+    private Map<T,Integer> vertices = new HashMap<>();//Map with pairs of vertices and indices
+    private int N = 0; //Graph size
+    private int alpha = 0;
     @Override
     public void addVertex(T x) {
-
+        if (x == null) return;
+        vertices.put(x,N++);
     }
 
     @Override
     public void removeVertex(T x) {
-
+        if(x == null) return;
+        if(!vertices.containsKey(x)) return;
+        vertices.remove(x); N--;
     }
 
     @Override
     public boolean hasVertex(T v) {
-        return false;
+        return vertices.containsKey(v);
     }
 
     @Override
     public List<T> getVertexes() {
-        return null;
+        return vertices.keySet().stream().toList();
     }
 
     @Override
-    public void addEdge(T v, T w) {
-
+    public void addEdge(T v, T w, int weight) {
+        if(!vertices.containsKey(v) || !vertices.containsKey(w) || weight <0) return;
+        int i = vertices.get(v), j = vertices.get(w);
+        costMatrix[i][j] = costMatrix[j][i] = weight;
+        alpha++;
     }
 
     @Override
     public void removeEdge(T v, T w) {
-
+        if(!vertices.containsKey(v) || !vertices.containsKey(w) || !hasEdge(v,w)) return;
+        int i = vertices.get(v), j = vertices.get(w);
+        costMatrix[i][j] = costMatrix[j][i] = -1; //Change costMatrix values in it.
+        alpha--;
     }
+
 
     @Override
     public boolean hasEdge(T v, T w) {
-        return false;
+        if(!vertices.containsKey(v) || !vertices.containsKey(w)) return false;
+        int i = vertices.get(v), j = vertices.get(w);
+        return costMatrix[i][j] == costMatrix[j][i] && costMatrix[i][j] >=0;
     }
 
     @Override
     public int order() {
-        return 0;
+        return N;
     }
 
     @Override
-    public int alpha() {
-        return 0;
-    }
+    public int alpha() {return alpha;}
 
     @Override
     public List<T> getAdjacencyList(T v) {
-        return null;
+
+        int i = vertices.get(v);
+        List<T> adjacencyList = new ArrayList<>(N);
+        List<T> vertexList = getVertexes();
+        for (T w: vertexList) {
+            int j = vertices.get(w);
+            if(costMatrix[i][j] >=0) adjacencyList.add(w);
+        }
+        return adjacencyList;
     }
+    //[[0,2,-1],
+    // [2,0,4],
+    // [-1,4,0] ]
+
+    private void resize(boolean upsize) {
+        // To avoid creating completely new matrices, we have a fixed
+        // array size, which is not fully used. Instead, we use more or
+        // less of it as size increases or increases.
+
+        // If the size of the array is not enough, this method expands it.
+
+        // If the size of the array is too large compared to the actual
+        // portion used, we downsize it.
+
+        int arraySize = costMatrix.length;
+        int[][] result;
+
+        if (upsize) {
+            result = new int[arraySize * 2][arraySize * 2];
+        } else {
+            result = new int[arraySize / 2][arraySize / 2];
+        }
+
+
+        int currentRow = 0;
+        int currentCol;
+        for (int i = 0; i < size; i++) {
+            if (costMatrix[0][i] == -1) continue;
+            currentCol = 0;
+
+            for (int j = 0; j < size; j++) {
+                if (costMatrix[i][j] == -1) continue;
+                result[currentRow][currentCol] = costMatrix[i][j];
+                currentCol++;
+            }
+            currentRow++;
+        }
+
+        costMatrix = result;
+    }
+
 }
